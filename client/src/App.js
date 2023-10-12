df57b6c16631b3d4768d184f6b724feec27041ee
anastasia <anyasirman@outlook.com>
2023-10-12 10:38:55 -0400

    conntected member route to front end and mapped members to p tags in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [members, setMembers] = useState([])
+
+  async function getMembers() {
+    let req = await fetch('/members')
+    let res = await req.json()
+    console.log(res.members)
+    setMembers(res.members)
+  }
+  useEffect(()=> {
+    getMembers()
+  }, [])
   return (
     <div className="App">
-      hello world
+      {
+       (typeof members === 'undefined') ? ( <p>Loading</p>): members.map((member, i) => <p key={i}>{member}</p>)
+      }
     </div>
   );
 }
